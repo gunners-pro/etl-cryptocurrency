38b42f6f033cc9a94766e5b994061e9b085a7ce3
Fabricyo Barreto <bass.barreto@gmail.com>
2025-12-03 21:45:35 -0300

    chore: change credential

--- scripts/create_container.py
+++ scripts/create_container.py
@@ -2,7 +2,7 @@ from azure.storage.blob import BlobServiceClient
 
 connection = BlobServiceClient(
     account_url="http://localhost:10000/devstoreaccount1",
-    credential="Eby8vdM02xNOcqFEYpYJys2OV8I6r9Q1qho="
+    credential="Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw=="
 )
 
 container_name = "datalake"
